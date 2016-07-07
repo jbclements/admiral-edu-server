619bb50675a67775966bf56772065d3ffd418b42
John Clements <clements@racket-lang.org>
2016-07-07 12:18:03 -0700

    omit zap splitting code to remove statistics dependency

--- admiral-edu-server/info.rkt
+++ admiral-edu-server/info.rkt
@@ -2,8 +2,8 @@
 
 (define name "Admiral Edu Server")
 
-#;(define compile-omit-paths '("experimental" "paper" "soups-paper" "user-study-2016"))
-#;(define test-omit-paths '("experimental" "paper" "soups-paper" "user-study-2016"))
+(define compile-omit-paths '("testing/splitting-zap-data.rkt"))
+(define test-omit-paths '("testing/splitting-zap-data.rkt"))
 #;(define scribblings '(("molis-hai.scrbl" () (tool))))
 
 #;(define raco-commands
