f5ecaa64fed8c8b7d49c4321dbebb5e1b787d841
John Clements <clements@racket-lang.org>
2016-07-06 23:35:14 -0700

    moving toward just using exceptions rather than the exception monad

--- admiral-edu-server/util/basic-types.rkt
+++ admiral-edu-server/util/basic-types.rkt
@@ -1,6 +1,7 @@
-#lang typed/racket
+#lang typed/racket/base
 
 
+;; FIXME: delete the Failure and Success structs and switch to exceptions
 (provide (struct-out Failure))
 (struct Failure ([message : String]) #:transparent)
 
@@ -14,5 +15,46 @@
 (: failure (String * -> Failure))
 (define (failure . messages)
   (Failure (apply string-append messages)))
+
+(provide (struct-out exn:user-error)
+         raise-400-bad-request
+         raise-404-not-found
+         raise-403-not-authorized)
+
+;; represents a user error (400, 403, 404)
+(struct exn:user-error exn ([code : Natural]) #:transparent)
+
+(: raise-400-bad-request (String -> Nothing))
+(define (raise-400-bad-request message)
+  (raise (exn:user-error message (current-continuation-marks) 400)))
+
+(: raise-404-not-found (String -> Nothing))
+(define (raise-404-not-found message)
+  (raise (exn:user-error message (current-continuation-marks) 404)))
+
+(: raise-403-not-authorized (String -> Nothing))
+(define (raise-403-not-authorized message)
+  (raise (exn:user-error message (current-continuation-marks) 403)))
+
+;; really need the exception monad here...
+
+;;; wait! why not just use actual racket exceptions? This whole success/fail thing seems silly.
+
+(provide sdo)
+(define-syntax sdo
+  (syntax-rules (<-)
+    [(_ [name <- rhs]) rhs]
+    [(_ rhs) rhs]
+    [(_ [name <- rhs] rest ...)
+     (let ([temp rhs])
+       (cond [(Success? temp)
+              (let ([name (Success-result temp)])
+                (sdo rest ...))]
+             [else temp]))]
+    [(_ rhs rest ...)
+     (let ([temp rhs])
+       (cond [(Success? temp)
+              (sdo rest ...)]
+             [else temp]))]))
 
 
