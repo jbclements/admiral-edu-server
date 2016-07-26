54f2c7f57e612d0a197e0af52c3333af65f71307
John Clements <clements@racket-lang.org>
2016-07-26 17:58:03 -0400

    add parameter to allow fixed user for testing

--- admiral-edu-server/auth/google-openidc.rkt
+++ admiral-edu-server/auth/google-openidc.rkt
@@ -5,15 +5,24 @@
                [exists-binding? (Symbol (Listof (Pairof Symbol String)) -> Boolean)]
                [request-headers (Any -> (Listof (Pairof Symbol String)))])
 
-(provide req->uid)
+(provide req->uid
+         fixed-user)
+
+;; this parameter is used for testing only. When it is not false,
+;; the server treats every request as
+;; being an authenticated one from a fixed user with the given
+;; email address.
+(: fixed-user (Parameter (U False String)))
+(define fixed-user (make-parameter #f))
 
 (define (req->uid req)
   (headers->uid (request-headers req)))
 
 (: headers->uid ((Listof (Pairof Symbol String)) -> (U String 'invalid-session)))
 (define (headers->uid headers)
-  (if (exists-binding? 'oidc_claim_email headers)
-      (extract-binding/single 'oidc_claim_email headers)
-      'invalid-session))
+  (or (fixed-user)
+      (if (exists-binding? 'oidc_claim_email headers)
+          (extract-binding/single 'oidc_claim_email headers)
+          'invalid-session)))
 
 
