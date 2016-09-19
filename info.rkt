5114c79eda3cf8b1b47aceedec721c272a7a813e
John Clements <clements@racket-lang.org>
2016-09-19 08:51:13 -0500

    split out tests into separate package

--- info.rkt
+++ info.rkt
@@ -3,20 +3,12 @@
 (define collection 'multi)
 
 (define deps
-  (list "base"
-        "db-lib"
-        "net-lib"
-        "rackunit-lib"
-        "typed-racket-lib"
-        "web-server-lib"
-        "aws"
-        "yaml"
-        "quickcheck"
-        ;; for testing:
-        "html-parsing"))
+  (list ))
 
 (define build-deps
-  (list "typed-racket-more"))
+  (list ))
+
+(define pkg-desc "The Racket Captain Teach server")
 
 
 
